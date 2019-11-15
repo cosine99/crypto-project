2c6c7003f3b365c27f035597f835a7c42f97c99e
PrakharGoenka <prakhar.goinka@gmail.com>
2019-11-15 23:50:51 +0530

    Implement proposed AESK phase.

--- web_app/app/algorithms/proposed/user.py
+++ web_app/app/algorithms/proposed/user.py
@@ -5,51 +5,73 @@ from foreign_agent import ForeignAgent
 class MobileUser:
 
     def __init__(self):
-        self.IDmu = 78
-
+        pass
 
     def registration(self, home_agent, foreign_agent):
-        print('Agent Registration Phase')
-        foreign_agent.agent_registration_1(home_agent)
+        print('Agent Registration Phase:')
+        foreign_agent.agent_registration_1(home_agent, self)
 
-        print("\nStep 1")
-        self.PWmu = int(input('MU chooses a password : '))
-        s = int(input('MU chooses a random number : ')     )
-        EID = xor(hash(xor(self.IDmu,self.PWmu,True)) ,s, True)
-        print("EID: \n", EID)
-        S = home_agent.user_registration_2(EID)
+        print('User Registration Phase:')
 
-        print("\nStep 3")        
-        self.SPW = xor(xor(S,hash(self.PWmu)), hash(orutil(self.IDmu, self.s)))
-        print("SPW: \n", self.SPW)
-        self.s1 = xor(self.s, hash(orutil(self.IDmu, self.PWmu)))
+        print('Step 1')
+        self.IDmu = input('Enter IDmu: ')
+        self.PWmu = input('Enter PWmu: ')
+        self.s = input('Enter a random number: ')
+
+        self.EID = xor(hash(xor(self.IDmu, self.PWmu)), self.s)
+        print('EID: ', self.EID)
+        self.S = home_agent.user_registration_2(self.EID)
+
+        print('Step 3') 
+
+        self.SPW = xor(xor(self.S, hash(self.PWmu)), hash(self.IDmu + self.s))
+        print('SPW: ', self.SPW)
+        self.s1 = xor(self.s, hash(self.IDmu + self.PWmu))
         print('Registration Completed')
         
 
-    def aesk(self, home_agent, foreign_agent):
-        print("\nStep 1")
-        print("pw of mu : \n", self.PWmu)
+    def aesk_step_1(self, home_agent, foreign_agent):
+        print('AESK Phase')
 
-        self.PW1mu = int(input('MU inputs password : '))
-        self.snew = int(input('MU selects a ran number : '))
-        self.Nm = int(input('MU selects a ran number : '))
-        s = xor(self.s1, hash(orutil(self.IDmu, self.PW1mu)))
-        self.EID1 = xor(hash(xor(self.IDmu,self.PW1mu)), s)
+        print('Step 1')
 
-        print("EID1: \n", self.EID1)
+        print('pw of mu : ', self.PWmu)
+        self.PW1mu = input('Enter password PW\'mu : ')
+        self.snew = input('MU selects a ran number : ')
+        self.Nm = input('MU selects a ran number : ')
+        s = xor(self.s1, hash(self.IDmu + self.PW1mu))
+        self.EID1 = xor(hash(xor(self.IDmu, self.PW1mu)), s)
 
-        self.S1 = xor(xor(self.SPW, hash(self.PW1mu)), hash(orutil(self.IDmu, s)))
+        print('EID1: ', self.EID1)
+
+        self.S1 = xor(xor(self.SPW, hash(self.PW1mu)), hash(self.IDmu + s))
         self.EIDnew = xor(hash(xor((self.IDmu), self.PW1mu)), self.snew)
-        self.Vm = xor(self.EIDnew, hash(orutil(self.S1,self.Nm)))
-        self.s1new = xor(self.snew, hash(orutil(self.IDmu, PW1mu)))
-        self.Qm = hash(orutil( (orutil(self.S1,self.Nm)), self.EIDnew))
+        self.Vm = xor(self.EIDnew, hash(self.S1 + self.Nm))
+        self.s1new = xor(self.snew, hash(self.IDmu + self.PW1mu))
+        self.Qm = str(hash(str(self.EIDnew) + str(self.S1) + str(self.Nm)))
 
-        print("S1: \n", self.S1)
-        print("EIDnew: \n", self.EIDnew)
-        print("Vm: \n", self.Vm)
-        print("Qm: \n", self.Qm)
+        print('S1: ', self.S1)
+        print('EIDnew: ', self.EIDnew)
+        print('Vm: ', self.Vm)
+        print('Qm: ', self.Qm)
 
-        foreign_agent.aesk_step2(home_agent, self.EID1, self.Vm, self.Qm, self.Nm)
+        foreign_agent.aesk_step_2(home_agent, self.EID1, self.Vm, self.Qm, self.Nm)
+
+    def aesk_step_5(self, Vf2, Qf2, Nf2, foreign_agent):
+        self.Snew = xor(Vf2, hash(str(self.S) + Nf2))
+
+        if(Qf2 == hash(self.EID + self.Snew + Nf2)):
+            print('Qf2 matches')
+        else:
+            print('Error at aesk_step_5')
+            return 0
+
+        self.SPWnew = xor(self.Snew, xor(hash(self.PWmu), hash(self.IDmu + self.s)))
+
+        self.Kmf = hash(self.Nm + Nf2 + str(self.S))
+        self.Qmf = hash(self.Nm + str(self.S) + Nf2 + self.Snew)
+
+        foreign_agent.aesk_step_6(self.Qmf)
 
         print('AESK Phase Completed')
 
@@ -63,7 +85,5 @@ if __name__ == '__main__':
     home_agent = HomeAgent()
     foreign_agent = ForeignAgent()
     user = MobileUser()
-    print('Registration Phase \n')
     user.registration(home_agent, foreign_agent)
-    print('AESK phase \n')
-    user.aesk(home_agent, foreign_agent)
+    user.aesk_step_1(home_agent, foreign_agent)
--- web_app/app/algorithms/proposed/utility.py
+++ web_app/app/algorithms/proposed/utility.py
@@ -4,22 +4,7 @@ import random
 from itertools import cycle
 import base64
 
-def hash(password):
-    # return hashlib.sha256(password.encode()).hexdigest()
-    return (abs(password))
+def xor(data, key):
 
-def xor(data, key, encode=False, decode=False):
-    
-    # if decode:
-    #      data = base64.decodestring(data)
-
-    # xored = ''.join(chr(ord(x) ^ ord(y)) for (x,y) in zip(data, cycle(key)))
-    # return xored
-
-    return (data ^ key)
-
-def orutil(data, key):
-    # ored = ''.join(chr(ord(x) | ord(y)) for (x,y) in zip(data, cycle(key)))
-    # return ored
-
-    return (data | key)
+    xored = ''.join(chr(ord(x) ^ ord(y)) for (x,y) in zip(str(data), cycle(str(key))))
+    return xored
