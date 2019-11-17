3658ecd43cd04143ee0a69e559f30049dd9157d2
PrakharGoenka <prakhar.goinka@gmail.com>
2019-11-18 00:49:19 +0530

    Update algorithm modules for integration with web app.

--- web_app/app/algorithms/proposed/foreign_agent.py
+++ web_app/app/algorithms/proposed/foreign_agent.py
@@ -1,83 +1,92 @@
 from utility import *
+from time import time
+
 
 class ForeignAgent:
+
     def __init__(self):
         self.SKfa = 'SKfa'
         self.IDfa = 'IDfa'
         self.b = 'b'
         self.Kfa = 'Kfa'
 
-    def agent_registration_1(self, home_agent, mobile_user):
-        self.mobile_user = mobile_user
+
+    def agent_registration(self, home_agent):
+        print('Agent Registration Phase:')
+
+        start = time()
+        # self.mobile_user = mobile_user
         self.Kfa = hash(self.IDfa + self.b)
-        home_agent.agent_registration_2(self.IDfa, self.Kfa, self)
+        status = home_agent.agent_registration_2(self.IDfa, self.Kfa, self)
+
+        end = time()
+        time_taken = end - start
+        print('Successful')
+
+        return status, time_taken
 
 
     def agent_registration_3(self, SKfa):
         self.SKfa = SKfa
 
+        return 0
 
-    def aesk_step_2(self, EID1, Vm, Qm, Nm, home_agent, mobile_user):
+
+    def aesk_step_2(self, EID1, Vm, Qm, Nm, Nf, Nf2, home_agent, mobile_user):        
         self.mobile_user = mobile_user;
-        print('Step 2')
-
         self.Qm = Qm
         self.EIDnew = EID1
         self.Nm = Nm
-        self.Nf = input('Foreign Agent chooses a rand number : ')
+        self.Nf = Nf
         self.Qf = hash((str(Qm) + (self.Nf) + str(self.SKfa)))
         self.Vf = xor(xor(self.Nf, hash(self.SKfa)), hash(str(self.Kfa) + str(Nm)))
         
-        print('Nf: ', self.Nf)
-        print('Qf: ', self.Qf)
-        print('Vf: ', self.Vf)
-        
-        home_agent.aesk_step_3(EID1, Vm, self.Vf, self.Qf, Nm, self.IDfa, self)
+        return home_agent.aesk_step_3(EID1, Vm, self.Vf, self.Qf, Nm, self.IDfa, Nf2, self)
 
-    def aesk_step_4(self, Vh, Snew, home_agent):
+
+    def aesk_step_4(self, Vh, Snew, Nf2, home_agent):
         self.Snew = Snew
-        temp = xor(Vh, hash(str(self.SKfa) + self.Nf))
-        print(str(temp))
-        if(self.Qm == str(hash(str(self.EIDnew) + str(Snew) + str(self.Nm)))): #JUGAAD HERE
-            print(' Qm matches')
-        else:
-            print('Failure at step 4')
-            return 0
 
-        self.Nf2 = input('Enter another random number')
+        if(self.Qm != str(hash(str(self.EIDnew) + str(Snew) + str(self.Nm)))): #JUGAAD HERE
+            print('Failure at step 4')
+            return 1
+
+        self.Nf2 = Nf2
         self.Vf2 = xor(Snew, hash(str(Snew) + self.Nf2))
         self.Qf2 = hash(self.EIDnew + str(self.Snew) + self.Nf2)
 
-        self.mobile_user.aesk_step_5(self.Vf2, self.Qf2, self.Nf2, self)
+        return self.mobile_user.aesk_step_5(self.Vf2, self.Qf2, self.Nf2, self)
+
 
     def aesk_step_6(self, Qmf):
         if(Qmf != hash(self.Nm + str(self.Snew) + self.Nf2 + str(self.Snew))):
             print('Qmf doesnt match')
-            return 0
+            return 1
 
         self.Kmf = hash(self.Nm + self.Nf2 + str(self.Snew))
 
-        return 1
+        return 0
 
-    def sk_update_2(self, Um, Qstarm, mobile_user):
+
+    def sk_update_2(self, Um, Qstarm, Nstarf, mobile_user):
         self.Nstarm = xor(Um, hash(str(self.Snew) + self.Nm + self.Nf2))
+
         if(Qstarm != hash(xor(self.Nstarm, self.Snew))):
             print('Qstarm doesnt match')
             return 0
 
-        self.Nstarf = input('New Nstarf')
-
+        self.Nstarf = Nstarf
         self.Uf = xor(self.Nstarf, hash(str(self.Snew) + self.Nf2 + self.Nstarm))
-
         self.Qstarf = hash(xor(self.Nstarf, self.Snew))
 
-        mobile_user.sk_update_3(self.Uf, self.Qstarf)
+        return mobile_user.sk_update_3(self.Uf, self.Qstarf)
 
 
     def sk_update_4(self, Qstarmf, mobile_user):
         if(Qstarmf != hash(xor(xor(self.Nstarm, self.Nstarf), self.Snew))):
             print('Qstarmf doesnt match')
-            return 0
+            return 1
 
         self.Kmf = mobile_user.Kmf
-        return 1
+        
+        return 0
--- web_app/app/algorithms/proposed/home_agent.py
+++ web_app/app/algorithms/proposed/home_agent.py
@@ -1,45 +1,43 @@
 from utility import *
 
+
 class HomeAgent:
     
     def __init__(self):
         self.SKha = 'SKha'
 
+
     def agent_registration_2(self, IDfa, Kfa, foreign_agent):
         self.SKfa = hash(xor(IDfa, self.SKha))
         self.Kfa = Kfa
-        foreign_agent.agent_registration_3(self.SKfa)
+
+        return foreign_agent.agent_registration_3(self.SKfa)
+
 
     def user_registration_2(self, EID):
-        print('Step 2')
-
         S = hash(EID + str(hash(self.SKha)))
-        print('S: ', S)
+        
+        return 0, S
 
-        return S
 
-    def aesk_step_3(self, EID1, Vm, Vf, Qf, Nm, IDfa, foreign_agent):
-        print('Step 3')
-        print(Qf)
+    def aesk_step_3(self, EID1, Vm, Vf, Qf, Nm, IDfa, Nf2, foreign_agent):
         self.S1 = hash(EID1 + str(hash(self.SKha)))
         self.EID1new = xor(Vm, hash(str(self.S1) + str(Nm)))
         SKfa = hash(xor(IDfa, self.SKha))
         self.Nf = xor(xor(Vf, hash(SKfa)), hash(str(self.Kfa) + str(Nm)))
         self.Qf = hash(str(hash(self.EID1new + str(self.S1) + Nm)) + self.Nf + str(SKfa))
-
-        if self.Qf==Qf:
-            print('QFs are Equal')
-        else:
+        # FIX THIS
+        if self.Qf!=Qf:
             print('QFs are Not Equal')
             self.Qf = Qf
 
         self.Snew = hash(str(self.EID1new) + str(hash(self.SKha))) 
         self.Vh = xor((self.EID1new + str(self.S1) + str(self.Snew)), hash(str(self.SKfa) + self.Nf))
 
-        foreign_agent.aesk_step_4(self.Vh, self.Snew, self)
+        return foreign_agent.aesk_step_4(self.Vh, self.Snew, Nf2, self)
 
 
     def password_altered_3(self, EID1new, mobile_user):
         self.Snew = hash(EID1new + str(hash(self.SKha)))
 
-        mobile_user.password_altered_4(self.Snew)
+        return mobile_user.password_altered_4(self.Snew)
